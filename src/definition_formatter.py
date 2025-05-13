f08b2fefa932f90720c13faa10e90c34e27b650e
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-13 19:11:18 +0700

    убрал коммиты

--- src/definition_formatter.py
+++ src/definition_formatter.py
@@ -4,81 +4,25 @@ import re
 class DefinitionFormatter:
     @staticmethod
     def format_definition(definition):
-        without_square_brackets = re.sub(r'\[\d+\]', '', definition)
-
-        without_before_the_dash = DefinitionFormatter.remove_to_dash(without_square_brackets)
-
-        without_after_dot = DefinitionFormatter.remove_after_dot(without_before_the_dash)
-
-        formatted_definition = DefinitionFormatter.upper_one_letter(without_after_dot, 0)
-
-        return formatted_definition
-    # def format_definition(definition):
-    #     print(f"Original definition: {definition}")
-    #
-    #     without_square_brackets = re.sub(r'\[\d+\]', '', definition)
-    #     print(f"Without square brackets: {without_square_brackets}")
-    #
-    #     without_before_the_dash = DefinitionFormatter.remove_to_dash(without_square_brackets)
-    #     print(f"Without before the dash: {without_before_the_dash}")
-    #
-    #     if not without_before_the_dash or len(without_before_the_dash) < 2:
-    #         print(f"Skipping formatting due to short or empty string: {without_before_the_dash}")
-    #         return without_before_the_dash
-    #
-    #     without_after_dot = DefinitionFormatter.remove_after_dot(without_before_the_dash)
-    #     print(f"Without after dot: {without_after_dot}")
-    #
-    #     if not without_after_dot or len(without_after_dot) < 2:
-    #         print(f"Skipping formatting due to short or empty string: {without_after_dot}")
-    #         return without_after_dot
-    #
-    #     formatted_definition = DefinitionFormatter.upper_one_letter(without_after_dot, 0)
-    #     print(f"Formatted definition: {formatted_definition}")
-    #
-    #     return formatted_definition
+        definition = re.sub(r'\[\d+]', '', definition)
+        definition = DefinitionFormatter._remove_text_before_dash(definition)
+        definition = DefinitionFormatter.remove_after_dot(definition)
+        definition = DefinitionFormatter.upper_one_letter(definition, 0)
+        return definition
 
     @staticmethod
-    @staticmethod
-    def remove_to_dash(text):
-        found_dash = False
-        inside_brackets = False
-        formatted_text = ''
-        temp_text = ''
-
-        for char in text:
-            if char == '(':
-                inside_brackets = True
-            elif char == ')':
-                inside_brackets = False
-            elif char == '—' and not inside_brackets and not found_dash:
-                found_dash = True
-                formatted_text += temp_text.strip()
-                temp_text = ''
-            elif inside_brackets:
-                continue
-            elif found_dash:
-                formatted_text += char
-            else:
-                temp_text += char
-
-        return formatted_text.strip() if found_dash else text.strip()
+    def _remove_text_before_dash(text):
+        dash_pos = text.find('—')
+        return text[dash_pos + 1:].strip() if dash_pos != -1 else text
 
     @staticmethod
     def remove_after_dot(text):
         match = re.search(r'\. [А-Я]', text)
-
-        if match:
-            formatted_text = text[:match.start() + 1].strip()
-        else:
-            formatted_text = text.strip()
-
-        return formatted_text
+        return text[:match.start() + 1].strip() if match else text
 
     @staticmethod
     def format_term(term):
         formatted_term = DefinitionFormatter.upper_one_letter(term, 0)
-        #formatted_term = re.sub(r'\([^()]*\)', '', formatted_term)
         return formatted_term
 
     @staticmethod
